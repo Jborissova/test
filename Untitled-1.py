247b9c58f83090635e41783e331b9fbca4172d2b
unknown <borissova706@gmail.com>
2020-02-13 00:59:53 -0400

    Second title

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+b = '23'
+
+html = open ("vivid.html", 'w')
+html_body = """
+	<html>
+		<head><title>abel</title></head>
+		<body>
+			<center><h1>{b}</h1></center>
+		</body>
+	</html>
+"""
+html.write(html_body)
+html.close()
